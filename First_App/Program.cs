528dabc7932625c0984469afa50626da380e4431
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-10-30 22:26:16 +0600

    Add project files.

---
+++ First_App/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace First_App
+{     
+
+      internal class Program
+      {
+            static void Main ( string [ ] args )
+            {
+                  int age =18;
+                  Console . WriteLine (age );
+
+            }
+
+
+
+      }
+
+
+}
